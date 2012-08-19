fa4310217b2eb815fbb67ac55c325006c317794f
Shawn Mclean <mixmasterxp@gmail.com>
2012-08-19 15:32:52 -0500

    postage to mandrill and version bump

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Self")]
-[assembly: AssemblyProduct("PostageApp")]
+[assembly: AssemblyProduct("Mandrill")]
 [assembly: AssemblyCopyright("Shawn Mclean (c) 2012")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
 
