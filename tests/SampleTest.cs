db624f4e8881309b791355f57fe388e770b490df
Shawn Mclean <mixmasterxp@gmail.com>
2012-08-19 16:38:36 -0500

    ...

--- tests/SampleTest.cs
+++ tests/SampleTest.cs
@@ -6,14 +6,7 @@ using System.Threading.Tasks;
 using Mandrill;
 using NUnit.Framework;
 
-namespace PostageApp.Tests
+namespace Mandrill.Tests
 {
-    [TestFixture]
-    public class SampleTest
-    {
-        [Test]
-        public void test()
-        {
-            }
-    }
+
 }
