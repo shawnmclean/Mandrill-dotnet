e20be78de91f693271c16429a4f8f5ced3731804
Shawn Mclean <mixmasterxp@gmail.com>
2012-09-02 22:16:46 -0500

    bump version

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Self")]
 [assembly: AssemblyProduct("Mandrill")]
 [assembly: AssemblyCopyright("Shawn Mclean (c) 2012")]
-[assembly: AssemblyVersion("0.0.0.3")]
-[assembly: AssemblyFileVersion("0.0.0.3")]
+[assembly: AssemblyVersion("0.0.0.6")]
+[assembly: AssemblyFileVersion("0.0.0.6")]
 
