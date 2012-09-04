a11ab4cca5145a989c43204e22dd27a8fc1c6f01
EliSchleifer <eli@directr.co>
2012-09-04 12:45:06 -0400

    UserInfo - full object specification
    1) Support for Async/Sync Info and Ping using fully structured objects.
    2) Refactor the MandrillApi class a little to simplify build out

--- src/Configuration.cs
+++ src/Configuration.cs
@@ -2,6 +2,7 @@
 {
     internal static class Configuration
     {
-        public static string BASE_URL = "https://mandrillapp.com/api/1.0/";
+        public static string BASE_SECURE_URL = "https://mandrillapp.com/api/1.0/";
+        public static string BASE_URL = "http://mandrillapp.com/api/1.0/";
     }
 }
---
+++ src/Utilities/MandrillException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandrill
+{
+    public class MandrillException : Exception
+    {
+        public ErrorResponse Error { get; private set; }
+
+        public MandrillException() { }
+
+        public MandrillException(string message)
+            : base(message)
+        {
+        }
+
+        public MandrillException(ErrorResponse error, string message)
+            : base(message)
+        {
+            this.Error = error;
+        }
+
+        public MandrillException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
