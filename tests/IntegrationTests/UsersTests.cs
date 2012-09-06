ae4357c69b4de09df325f29d6082b902f14062c3
Shawn Mclean <mixmasterxp@gmail.com>
2012-09-05 20:32:44 -0500

    Added app settings for storing test config

---
+++ tests/IntegrationTests/UsersTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Mandrill.Tests.IntegrationTests
+{
+    [TestFixture]
+    public class UsersTests
+    {
+        [Test]
+        public void lol()
+        {
+            var apiKey = ConfigurationManager.AppSettings["APIKey"];
+
+        }
+    }
+}
