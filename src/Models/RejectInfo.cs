ba4d9a82a77c7e3cfcc9043ca0225f9f0c625889
Shawn Mclean <mixmasterxp@gmail.com>
2012-09-09 14:09:33 -0500

    Added rejects info model and List Rejects api call.

--- src/Models/RejectInfo.cs
+++ src/Models/RejectInfo.cs
@@ -4,7 +4,22 @@ namespace Mandrill
 {
     public class RejectInfo
     {
+        [JsonProperty("email")]
+        public string Email { get; set; }
         
+        [JsonProperty("reason")]
+        public string Reason { get; set; }
+
+        [JsonProperty("created_at")]
+        public string CreatedAt { get; set; }
+
+        [JsonProperty("expires_at")]
+        public string ExpiresAt { get; set; }
+
+        [JsonProperty("expired")]
+        public bool Expired { get; set; }
+        
+        //TODO: Add property: Sender: "Sender":{"...": "..."}
     }
 
     public class RejectDeleteResult
--- src/Rejects.cs
+++ src/Rejects.cs
@@ -23,7 +23,36 @@ namespace Mandrill
         public Task<RejectDeleteResult> DeleteRejectAsync(string email)
         {
             var path = "/rejects/delete.json";
-            return PostAsync<RejectDeleteResult>(path, null);
+            return PostAsync<RejectDeleteResult>(path, new {email});
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="email">email address to limit the results</param>
+        /// <returns></returns>
+        public List<RejectInfo> ListRejects(string email)
+        {
+            try
+            {
+                return ListRejectsAsync(email).Result;
+            }catch(AggregateException aex)
+            {
+                //catch and throw the inner exception
+                throw aex.Flatten().InnerException;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="email">email address to limit the results</param>
+        /// <returns></returns>
+        public Task<List<RejectInfo>> ListRejectsAsync(string email)
+        {
+            var path = "/rejects/list.json";
+            return PostAsync<List<RejectInfo>>(path, new {email});
+        }
+
     }
 }
