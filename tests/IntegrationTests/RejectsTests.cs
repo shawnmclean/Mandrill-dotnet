405eff9d02e1fd6495a620c2dfc553564dab3fd2
Shawn Mclean <mixmasterxp@gmail.com>
2012-09-09 22:31:20 -0500

    wrote tests for reject listing. (works on my api key :))

--- tests/IntegrationTests/RejectsTests.cs
+++ tests/IntegrationTests/RejectsTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,49 @@ namespace Mandrill.Tests.IntegrationTests
             // Verify
             var ex = Assert.Throws<MandrillException>(() => api.DeleteReject(reject));
             Assert.That(ex.Error.name, Is.EqualTo("Invalid_Key"));
+        }
+
+        [Test]
+        public void Delete_Reject_Throws_Exception_On_Invalid_Reject()
+        {
+            // Setup
+            var apiKey = ConfigurationManager.AppSettings["APIKey"];
+            string reject = "test@test.com";
+
+            // Exercise
+            var api = new MandrillApi(apiKey);
+
+            //Verify
+            var ex = Assert.Throws<MandrillException>(() => api.DeleteReject(reject));
+            Assert.That(ex.Error.name, Is.EqualTo("Invalid_Reject"));
+        }
+
+        [Test]
+        public void List_Rejects_Throws_Exception_On_Invalid_ApiKey()
+        {
+            // Setup
+            var apiKey = " ";
+
+            // Exercise
+            var api = new MandrillApi(apiKey);
+
+            // Verify
+            var ex = Assert.Throws<MandrillException>(() => api.ListRejects());
+            Assert.That(ex.Error.name, Is.EqualTo("Invalid_Key"));
+        }
+
+        [Test]
+        public void List_Rejects_Works()
+        {
+            // Setup
+            var apiKey = ConfigurationManager.AppSettings["APIKey"];
+
+            // Exercise
+            var api = new MandrillApi(apiKey);
+
+            // Verify
+            var rejects = api.ListRejects();
+            //TODO: test this....
         }
     }
 }
