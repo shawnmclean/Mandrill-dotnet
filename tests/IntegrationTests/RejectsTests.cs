712509dec74dcc4755abe9d738ac5f01a538e087
Shawn Mclean <mixmasterxp@gmail.com>
2012-09-09 22:18:23 -0500

    wrote test for delete reject throwing exception on invalid key.

---
+++ tests/IntegrationTests/RejectsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Mandrill.Tests.IntegrationTests
+{
+    [TestFixture]
+    public class RejectsTests
+    {
+        [Test]
+        public void Delete_Reject_Throws_Exception_On_Invalid_ApiKey()
+        {
+            // Setup
+            var apiKey = " ";
+            string reject = "test@test.com";
+
+            // Exercise
+            var api = new MandrillApi(apiKey);
+
+            // Verify
+            var ex = Assert.Throws<MandrillException>(() => api.DeleteReject(reject));
+            Assert.That(ex.Error.name, Is.EqualTo("Invalid_Key"));
+        }
+    }
+}
