65419f1b6cfbb21058fc692fd8f49e4816c61c66
Shawn Mclean <mixmasterxp@gmail.com>
2012-09-15 18:52:05 -0500

    Version bump to fix sending emails with templates.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Self")]
 [assembly: AssemblyProduct("Mandrill")]
 [assembly: AssemblyCopyright("Shawn Mclean (c) 2012")]
-[assembly: AssemblyVersion("0.0.0.9")]
-[assembly: AssemblyFileVersion("0.0.0.9")]
+[assembly: AssemblyVersion("0.0.0.10")]
+[assembly: AssemblyFileVersion("0.0.0.10")]
