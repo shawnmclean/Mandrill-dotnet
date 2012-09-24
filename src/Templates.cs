a1a684edec0d8299fad4bf2b46e3ef3e9b8e2097
markokristian <marko.helin@payson.se>
2012-09-24 18:03:24 +0200

    Added support to render a template with template content and merge vars.

--- src/Templates.cs
+++ src/Templates.cs
@@ -1,11 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using System.Threading.Tasks;
+using Mandrill.Models;
+using RestSharp;
 
 namespace Mandrill
 {
     public partial class MandrillApi
     {
+        public RenderedTemplate Render(string templateName, IEnumerable<TemplateContent> templateContents, IEnumerable<merge_var> mergeVars)
+        {
+            return RenderAsync(templateName, templateContents, mergeVars).Result;
+        }
+
+        public Task<RenderedTemplate> RenderAsync(string templateName, IEnumerable<TemplateContent> templateContents, IEnumerable<merge_var> mergeVars)
+        {
+            const string path = "/templates/render.json";
+
+            dynamic payload = new ExpandoObject();
+
+            payload.template_name = templateName;
+            payload.template_content = templateContents;
+            payload.merge_vars = mergeVars;
+
+            Task<IRestResponse> post = PostAsync(path, payload);
+
+            return post.ContinueWith(p =>
+            {
+                return JSON.Parse<RenderedTemplate>(p.Result.Content);
+            }, TaskContinuationOptions.ExecuteSynchronously);
+        }
     }
 }
