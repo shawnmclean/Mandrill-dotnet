f666bac645831bb1f808ccdbad7921477f3f33c4
Shawn Mclean <mixmasterxp@gmail.com>
2012-09-25 19:02:28 -0500

    Wrote a test for rendering template successfully.

---
+++ tests/IntegrationTests/TemplatesTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Mandrill.Tests.IntegrationTests
+{[TestFixture]
+    class TemplatesTests
+    {
+           
+        [Test]
+        public void Render_Template_Returns_Correct_Content()
+        {
+            // Setup
+            var apiKey = ConfigurationManager.AppSettings["APIKey"];
+            var templateName = ConfigurationManager.AppSettings["TemplateExample"];
+            
+            // Exercise
+            var api = new MandrillApi(apiKey);
+            var templateContent = new TemplateContent
+                                      {
+                                          content = "Test",
+                                          name = "model1"
+                                      };
+            var result = api.Render(templateName, new List<TemplateContent>{templateContent}, null);
+
+            string expected = "<!DOCTYPE html>\n<span>Test</span>";
+            
+            // Verify
+            Assert.AreEqual(expected, result.html);
+        }
+    }
+}
