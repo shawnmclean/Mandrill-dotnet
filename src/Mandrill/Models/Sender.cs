b2609ad4439c13aac62f3ff58ad12e847461598c
Shawn Mclean <mixmasterxp@gmail.com>
2013-03-16 16:18:44 -0500

    Added Senders List

---
+++ src/Mandrill/Models/Sender.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mandrill.Models
+{
+    public class Sender
+    {
+        public string address { get; set; }
+        public string created_at { get; set; }
+        public int sent { get; set; }
+        public int hard_bounces { get; set; }
+        public int soft_bounces { get; set; }
+        public int rejects { get; set; }
+        public int complaints { get; set; }
+        public int unsubs { get; set; }
+        public int opens { get; set; }
+        public int clicks { get; set; }
+    }
+}
--- src/Mandrill/Senders.cs
+++ src/Mandrill/Senders.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using Mandrill.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Mandrill
 {
     public partial class MandrillApi
     {
+
+        public Task<List<Sender>> ListSendersAsync()
+        {
+            const string path = "/senders/list.json";
+
+            return this.PostAsync(path, null).ContinueWith(
+                p => JSON.Parse<List<Sender>>(p.Result.Content),
+                TaskContinuationOptions.ExecuteSynchronously);
+        }
+
+        public List<Sender> LisSenders()
+        {
+            return this.ListSendersAsync().Result;
+        }
     }
 }
