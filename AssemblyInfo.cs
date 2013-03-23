c04894b2690cb1259dc7da934a1b3221eea95b58
Shawn Mclean <mixmasterxp@gmail.com>
2013-03-23 17:09:55 -0500

    version bump for senders list.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Self")]
 [assembly: AssemblyProduct("Mandrill")]
 [assembly: AssemblyCopyright("Shawn Mclean (c) 2012")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
