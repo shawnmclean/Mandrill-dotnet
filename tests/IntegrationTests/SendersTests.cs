67a02b73bb656490fb137c4f17753b90b077044f
Shawn Mclean <mixmasterxp@gmail.com>
2013-03-23 16:51:11 -0500

    added senders test structure.

---
+++ tests/IntegrationTests/SendersTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Mandrill.Tests.IntegrationTests
+{
+    [TestFixture]
+    public class SendersTests
+    {
+        public static bool Validator(object sender, X509Certificate certificate, X509Chain chain,
+                                      SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            if (ConfigurationManager.AppSettings["IgnoreInvalidSSLCertificate"] == "True")
+                ServicePointManager.ServerCertificateValidationCallback = Validator;
+        }
+
+        [Test]
+        public void Senders_Returns_List()
+        {
+            // Setup
+            var apiKey = ConfigurationManager.AppSettings["APIKey"];
+
+            // Exercise
+            var api = new MandrillApi(apiKey);
+
+            // Verify
+            //var senders = api.ListSenders();
+            
+        }
+    }
+}
