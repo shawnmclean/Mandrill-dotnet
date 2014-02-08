f56eedb762f08b7346eaa23e9178d04d24ec8909
Shawn Mclean <shawnrmclean@gmail.com>
2014-02-08 14:44:39 -0500

    Added host logging to build script.

--- scripts/TokenizeConfiguration.ps1
+++ scripts/TokenizeConfiguration.ps1
@@ -2,15 +2,27 @@ Param(
     $filePath = "",
     [hashtable]$lookupTable = @{}
 )
+$readFile = "$filePath/tests/AppSettings.example.config"
+$saveFile = "$filePath/tests/AppSettings.config"
 
-[xml]$xml = gc "$filePath/tests/AppSettings.example.config"
+Write-Host "Looking for file: $readFile"
+
+[xml]$xml = gc $readFile
+
+Write-Host "File read into memory: $readFile"
+
 $xml.appSettings.add | ForEach-Object {
     $element = $_
     $lookupTable.GetEnumerator() | ForEach-Object {
         if ($element.key -eq $_.Key)
         {
             $element.value = $_.Value
+
+            Write-Host "Replacing key: " $element.key
         }
     } 
 }
-$xml.Save("$filePath/tests/AppSettings.config")
+
+Write-Host "Saving file: $saveFile"
+
+$xml.Save($saveFile)
