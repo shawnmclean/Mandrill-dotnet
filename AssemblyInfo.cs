869cc84ed9f3bac10810ce0fe0d2bf6063de820a
Shawn Mclean <shawnrmclean@gmail.com>
2014-02-09 04:00:32 -0500

    Removed assemblyinfo and rakefile

--- AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyCompany("Self")]
-[assembly: AssemblyProduct("Mandrill")]
-[assembly: AssemblyCopyright("Shawn Mclean (c) 2013")]
-[assembly: AssemblyVersion("0.1.1.4")]
-[assembly: AssemblyFileVersion("0.1.1.4")]
