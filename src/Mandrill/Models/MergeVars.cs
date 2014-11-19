08265b61dd4fc0b6ec85f8ba6d1b71f4a493b4d3
Shawn Mclean <shawnrmclean@gmail.com>
2014-11-19 00:08:24 -0500

    Added nuspec directly to project, will create nuget package on build.

---
+++ src/Mandrill/Models/MergeVars.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mandrill.Models
+{
+    public class MergeVars
+    {
+        public string rcpt { get; set; }
+        public IEnumerable<TemplateContent> vars { get; set; }
+    }
+}
---
+++ src/Mandrill/Models/TemplateType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mandrill.Models
+{
+    public enum TemplateType
+    {
+        MergeTags,
+        GlobalMergeTags,
+        EditableRegion
+    }
+}
