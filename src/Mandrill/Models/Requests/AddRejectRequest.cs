47d9acda94901f244f2ce4a1930dfd60f4ef9977
Shawn Mclean <shawnrmclean@gmail.com>
2015-05-01 02:26:18 -0500

    Add Reject request model added.

---
+++ src/Mandrill/Models/Requests/AddRejectRequest.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace Mandrill.Models.Requests
+{
+  /// <summary>
+  /// Class AddRejectRequest.
+  /// </summary>
+  public class AddRejectRequest : RequestBase
+  {
+    /// <summary>
+    /// Gets or sets the email.
+    /// </summary>
+    /// <value>The email.</value>
+    [JsonProperty("email")]
+    public string Email { get; set; }
+    /// <summary>
+    /// Gets or sets the comment.
+    /// </summary>
+    /// <value>The comment.</value>
+    [JsonProperty("comment")]
+    public string Comment { get; set; }
+    /// <summary>
+    /// Gets or sets the subaccount.
+    /// </summary>
+    /// <value>The subaccount.</value>
+    [JsonProperty("subaccount")]
+    public string SubAccount { get; set; }
+  }
+}
