66d6b30d22ee3f4747f7a09420e32c6f21068862
Shawn Mclean <shawnrmclean@gmail.com>
2015-05-02 22:38:36 -0500

    delete subaccount refactored. See #127

---
+++ src/Mandrill/Models/Requests/DeleteSubAccountRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Mandrill.Models.Requests
+{
+  public class DeleteSubAccountRequest : RequestBase
+  {
+    public DeleteSubAccountRequest(string id)
+    {
+      Id = id;
+    }
+
+    [JsonProperty("id")]
+    public string Id { get; set; }
+  }
+}
