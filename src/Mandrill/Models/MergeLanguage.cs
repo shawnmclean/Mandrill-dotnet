77ec5beff37198216422bbb88c6462b944e3b2e7
Ronnie Overby <rdodontplay@gmail.com>
2015-08-02 11:08:44 -0400

    Create MergeLanguage.cs

---
+++ src/Mandrill/Models/MergeLanguage.cs
@@ -0,0 +1,8 @@
+namespace Mandrill.Models
+{
+    public static class MergeLanguage
+    {
+        public const string Handlebars = "handlebars";
+        public const string MailChimp = "mailchimp";
+    }
+}
