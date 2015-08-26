78a269494bcf4c31ac7dbeb783c9acd19444334f
ellern <martin.ellern.bilgrau@gmail.com>
2015-08-26 14:35:44 +0200

    Document formatting

--- src/Mandrill/MandrillApi.cs
+++ src/Mandrill/MandrillApi.cs
@@ -18,99 +18,99 @@ using Newtonsoft.Json;
 
 namespace Mandrill
 {
+  /// <summary>
+  ///   Core class for using the MandrillApp Api
+  /// </summary>
+  public partial class MandrillApi
+  {
+    #region Fields
+
+    private readonly string baseUrl;
+    private HttpClient _httpClient;
+
+    #endregion
+
+    #region Constructors and Destructors
+
     /// <summary>
-    ///   Core class for using the MandrillApp Api
+    ///   Initializes a new instance of the <see cref="MandrillApi" /> class.
     /// </summary>
-    public partial class MandrillApi
+    /// <param name="apiKey">
+    ///   The API Key recieved from MandrillApp
+    /// </param>
+    /// <param name="useHttps">
+    /// </param>
+    /// <param name="timeout">
+    ///   Timeout in milliseconds to use for requests.
+    /// </param>
+    public MandrillApi(string apiKey, bool useHttps = true)
     {
-        #region Fields
+      ApiKey = apiKey;
 
-        private readonly string baseUrl;
-        private HttpClient _httpClient;
-
-        #endregion
-
-        #region Constructors and Destructors
-
-        /// <summary>
-        ///   Initializes a new instance of the <see cref="MandrillApi" /> class.
-        /// </summary>
-        /// <param name="apiKey">
-        ///   The API Key recieved from MandrillApp
-        /// </param>
-        /// <param name="useHttps">
-        /// </param>
-        /// <param name="timeout">
-        ///   Timeout in milliseconds to use for requests.
-        /// </param>
-        public MandrillApi(string apiKey, bool useHttps = true)
-        {
-            ApiKey = apiKey;
-
-            if (useHttps)
-            {
-                baseUrl = Configuration.BASE_SECURE_URL;
-            }
-            else
-            {
-                baseUrl = Configuration.BASE_URL;
-            }
-        }
-
-        #endregion
-
-        #region Public Properties
-
-        /// <summary>
-        ///   The Api Key for the project received from the MandrillApp website
-        /// </summary>
-        public string ApiKey { get; private set; }
-
-        #endregion
-
-        #region Public Methods and Operators
-
-        /// <summary>
-        ///   Allows overriding the HttpClient which is used in Post()
-        /// </summary>
-        /// <param name="httpClient">the httpClient to use</param>
-        public void SetHttpClient(HttpClient httpClient)
-        {
-            _httpClient = httpClient;
-        }
-
-        /// <summary>
-        ///   Execute post to path
-        /// </summary>
-        /// <param name="path">the path to post to</param>
-        /// <param name="data">the payload to send in request body as json</param>
-        /// <returns></returns>
-        public async Task<T> Post<T>(string path, RequestBase data)
-        {
-            data.Key = ApiKey;
-            try
-            {
-                using (var client = _httpClient ?? new HttpClient())
-                {
-                    client.BaseAddress = new Uri(baseUrl);
-
-                    var response = await client.PostAsync(path, new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json")).ConfigureAwait(false);
-
-                    response.EnsureSuccessStatusCode();
-
-                    return JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync().ConfigureAwait(false));
-                }
-            }
-            catch (TimeoutException)
-            {
-                throw new TimeoutException(string.Format("Post timed out to {0}", path));
-            }
-            catch (HttpRequestException ex)
-            {
-                throw new MandrillException(string.Format("Post failed {0} with status {1} and content '{2}'", path, ex.ToString(), data));
-            }
-        }
-
-        #endregion
+      if (useHttps)
+      {
+        baseUrl = Configuration.BASE_SECURE_URL;
+      }
+      else
+      {
+        baseUrl = Configuration.BASE_URL;
+      }
     }
+
+    #endregion
+
+    #region Public Properties
+
+    /// <summary>
+    ///   The Api Key for the project received from the MandrillApp website
+    /// </summary>
+    public string ApiKey { get; private set; }
+
+    #endregion
+
+    #region Public Methods and Operators
+
+    /// <summary>
+    ///   Allows overriding the HttpClient which is used in Post()
+    /// </summary>
+    /// <param name="httpClient">the httpClient to use</param>
+    public void SetHttpClient(HttpClient httpClient)
+    {
+      _httpClient = httpClient;
+    }
+
+    /// <summary>
+    ///   Execute post to path
+    /// </summary>
+    /// <param name="path">the path to post to</param>
+    /// <param name="data">the payload to send in request body as json</param>
+    /// <returns></returns>
+    public async Task<T> Post<T>(string path, RequestBase data)
+    {
+      data.Key = ApiKey;
+      try
+      {
+        using (var client = _httpClient ?? new HttpClient())
+        {
+          client.BaseAddress = new Uri(baseUrl);
+
+          var response = await client.PostAsync(path, new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json")).ConfigureAwait(false);
+
+          response.EnsureSuccessStatusCode();
+
+          return JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync().ConfigureAwait(false));
+        }
+      }
+      catch (TimeoutException)
+      {
+        throw new TimeoutException(string.Format("Post timed out to {0}", path));
+      }
+      catch (HttpRequestException ex)
+      {
+        throw new MandrillException(string.Format("Post failed {0} with status {1} and content '{2}'", path, ex.ToString(), data));
+      }
+    }
+
+    #endregion
+  }
 }
--- tests/UnitTests/PostTests.cs
+++ tests/UnitTests/PostTests.cs
@@ -11,128 +11,128 @@ using System.Threading.Tasks;
 
 namespace Mandrill.Tests.UnitTests
 {
-    [TestFixture]
-    public class PostTests
+  [TestFixture]
+  public class PostTests
+  {
+    [SetUp]
+    public void CreateHttpTest()
     {
-        [SetUp]
-        public void CreateHttpTest()
-        {
-            var responseMessage = new HttpResponseMessage();
-            var messageHandler = new FakeHttpMessageHandler(responseMessage);
-            httpClient = new HttpClient(messageHandler);
-        }
+      var responseMessage = new HttpResponseMessage();
+      var messageHandler = new FakeHttpMessageHandler(responseMessage);
+      httpClient = new HttpClient(messageHandler);
+    }
 
-        [TearDown]
-        public void DisposeHttpTest()
-        {
-            httpClient.Dispose();
-        }
+    [TearDown]
+    public void DisposeHttpTest()
+    {
+      httpClient.Dispose();
+    }
 
-        private void RespondWith(MandrillApi api, HttpStatusCode statusCode, string content)
-        {
-            var responseMessage = new HttpResponseMessage(statusCode);
-            responseMessage.Content = new FakeHttpContent(content);
-            var messageHandler = new FakeHttpMessageHandler(responseMessage);
-            api.SetHttpClient(new HttpClient(messageHandler));
-        }
+    private void RespondWith(MandrillApi api, HttpStatusCode statusCode, string content)
+    {
+      var responseMessage = new HttpResponseMessage(statusCode);
+      responseMessage.Content = new FakeHttpContent(content);
+      var messageHandler = new FakeHttpMessageHandler(responseMessage);
+      api.SetHttpClient(new HttpClient(messageHandler));
+    }
 
-        private HttpClient httpClient;
+    private HttpClient httpClient;
 
-        private class SampleObject
-        {
-            public string Name { get; set; }
-            public int Id { get; set; }
-        }
+    private class SampleObject
+    {
+      public string Name { get; set; }
+      public int Id { get; set; }
+    }
 
-        private class SamplePayload : RequestBase
-        {
-        }
+    private class SamplePayload : RequestBase
+    {
+    }
 
-        [Test]
-        public async Task Should_Serialize_Response_When_Json_Content_Is_Recieved()
-        {
-            string responseString = @"{
+    [Test]
+    public async Task Should_Serialize_Response_When_Json_Content_Is_Recieved()
+    {
+      string responseString = @"{
 	      ""Name"": ""Shawn"",
 	      ""Id"": 1
       }";
-            var api = new MandrillApi("");
-            RespondWith(api, HttpStatusCode.OK, responseString);
+      var api = new MandrillApi("");
+      RespondWith(api, HttpStatusCode.OK, responseString);
 
-            SampleObject response = await api.Post<SampleObject>("", new SamplePayload());
+      SampleObject response = await api.Post<SampleObject>("", new SamplePayload());
 
-            Assert.AreEqual("Shawn", response.Name);
-            Assert.AreEqual(1, response.Id);
-        }
+      Assert.AreEqual("Shawn", response.Name);
+      Assert.AreEqual(1, response.Id);
+    }
 
-        [Test]
-        public async Task Should_Throw_Mandrill_Exception_When_Server_Error()
-        {
-            string responseString = @"{
+    [Test]
+    public async Task Should_Throw_Mandrill_Exception_When_Server_Error()
+    {
+      string responseString = @"{
 	      ""code"": ""501"",
 	      ""message"": ""m1"",
 	      ""name"": ""n1"",
 	      ""status"": ""s1""
       }";
 
-            var api = new MandrillApi("");
-            RespondWith(api, HttpStatusCode.InternalServerError, responseString);
+      var api = new MandrillApi("");
+      RespondWith(api, HttpStatusCode.InternalServerError, responseString);
 
-            var ex = Assert.Throws<MandrillException>(async () => await api.Post<object>("", new SamplePayload()));
-            Assert.AreEqual(501, ex.Error.Code);
-            Assert.AreEqual("m1", ex.Error.Message);
-            Assert.AreEqual("n1", ex.Error.Name);
-            Assert.AreEqual("s1", ex.Error.Status);
-        }
-
-        // TODO: Figure out how to Simulate a timeout using HttpClient
-        //[Test]
-        //public async Task Should_Throw_TimeOut_Exception_When_Timing_Out()
-        //{
-        //    httpClient.SimulateTimeout();
-
-        //    var api = new MandrillApi("");
-
-        //    Assert.Throws<TimeoutException>(async () => await api.Post<object>("", new SamplePayload()));
-        //}
-
-        public class FakeHttpMessageHandler : HttpMessageHandler
-        {
-            private HttpResponseMessage response;
-
-            public FakeHttpMessageHandler(HttpResponseMessage response)
-            {
-                this.response = response;
-            }
-
-            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-            {
-                var responseTask = new TaskCompletionSource<HttpResponseMessage>();
-                responseTask.SetResult(response);
-
-                return responseTask.Task;
-            }
-        }
-
-        public class FakeHttpContent : HttpContent
-        {
-            public string Content { get; set; }
-
-            public FakeHttpContent(string content)
-            {
-                Content = content;
-            }
-
-            protected async override Task SerializeToStreamAsync(Stream stream, TransportContext context)
-            {
-                byte[] byteArray = Encoding.ASCII.GetBytes(Content);
-                await stream.WriteAsync(byteArray, 0, Content.Length).ConfigureAwait(false);
-            }
-
-            protected override bool TryComputeLength(out long length)
-            {
-                length = Content.Length;
-                return true;
-            }
-        }
+      var ex = Assert.Throws<MandrillException>(async () => await api.Post<object>("", new SamplePayload()));
+      Assert.AreEqual(501, ex.Error.Code);
+      Assert.AreEqual("m1", ex.Error.Message);
+      Assert.AreEqual("n1", ex.Error.Name);
+      Assert.AreEqual("s1", ex.Error.Status);
     }
+
+    // TODO: Figure out how to Simulate a timeout using HttpClient
+    //[Test]
+    //public async Task Should_Throw_TimeOut_Exception_When_Timing_Out()
+    //{
+    //    httpClient.SimulateTimeout();
+
+    //    var api = new MandrillApi("");
+
+    //    Assert.Throws<TimeoutException>(async () => await api.Post<object>("", new SamplePayload()));
+    //}
+
+    public class FakeHttpMessageHandler : HttpMessageHandler
+    {
+      private HttpResponseMessage response;
+
+      public FakeHttpMessageHandler(HttpResponseMessage response)
+      {
+        this.response = response;
+      }
+
+      protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+      {
+        var responseTask = new TaskCompletionSource<HttpResponseMessage>();
+        responseTask.SetResult(response);
+
+        return responseTask.Task;
+      }
+    }
+
+    public class FakeHttpContent : HttpContent
+    {
+      public string Content { get; set; }
+
+      public FakeHttpContent(string content)
+      {
+        Content = content;
+      }
+
+      protected async override Task SerializeToStreamAsync(Stream stream, TransportContext context)
+      {
+        byte[] byteArray = Encoding.ASCII.GetBytes(Content);
+        await stream.WriteAsync(byteArray, 0, Content.Length).ConfigureAwait(false);
+      }
+
+      protected override bool TryComputeLength(out long length)
+      {
+        length = Content.Length;
+        return true;
+      }
+    }
+  }
 }
