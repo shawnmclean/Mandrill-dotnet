dd228c0d2de7448bc7411427ee90cff09a4a9a1a
Andrew Carey <spamsucks912@gmail.com>
2016-05-26 15:20:05 -0700

    Update Configuration.cs
    Added static API URLs.

--- src/Mandrill/Configuration.cs
+++ src/Mandrill/Configuration.cs
@@ -27,6 +27,16 @@ namespace Mandrill
     public static string BASE_URL = "http://mandrillapp.com/api/1.0/";
 
     /// <summary>
+    ///   The bas e_ secur e_ url.
+    /// </summary>
+    public static string BASE_STATIC_SECURE_URL = "https://static.mandrillapp.com/api/1.0/";
+
+    /// <summary>
+    ///   The bas e_ url.
+    /// </summary>
+    public static string BASE_STATIC_URL = "http://static.mandrillapp.com/api/1.0/";
+
+    /// <summary>
     ///   The dat e_ tim e_ forma t_ string.
     /// </summary>
     public static string DATE_TIME_FORMAT_STRING = "yyyy-MM-dd HH:mm:ss";
