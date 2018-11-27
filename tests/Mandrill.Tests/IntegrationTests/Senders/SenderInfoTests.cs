24dfb3489813ad13f139a43c529b68990ea4b0b7
MarcusS <marcuss@payscale.com>
2018-11-27 09:39:58 -0800

    add integration test

---
+++ tests/Mandrill.Tests/IntegrationTests/Senders/SenderInfoTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Mandrill.Models;
+using Mandrill.Requests.Senders;
+using Xunit;
+
+namespace Mandrill.Tests.IntegrationTests.Senders
+{
+  [TestFixture]
+  public class SenderInfoTests
+  {
+    [Test]
+    public async Task Should_Return_Sender()
+    {
+        // Setup
+        string apiKey = ConfigurationManager.AppSettings["APIKey"];
+        
+        // Exercise
+        var api = new MandrillApi(apiKey);
+
+        // Verify
+        Sender sender = await api.SenderInfo(new SenderInfoRequest("testaddress@test.com"));
+
+        Assert.IsNotNull(sender);
+    }
+  }
+}
